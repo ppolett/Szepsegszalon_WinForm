dcba26efcf972ee3481412f1c6e2ea29fbb7f427
ppolett <pardi.polett@gmail.com>
2021-12-13 20:51:29 +0100

    Szűrő de hibás még

--- Szepsegszalon_WinForm_PPP/Szepsegszalon_WinForm_PPP/Form1.Designer.cs
+++ Szepsegszalon_WinForm_PPP/Szepsegszalon_WinForm_PPP/Form1.Designer.cs
@@ -36,18 +36,25 @@ namespace Szepsegszalon_WinForm_PPP
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.kezelésMegnevezéseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.alkalmazottNeveDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.alkalmazottSzakmájaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.kezeltVendégNeveDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.kezelésIdőpontjaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.szakmakBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.szakmakTableAdapter = new Szepsegszalon_WinForm_PPP.szepsegszalonDataSetTableAdapters.szakmakTableAdapter();
             this.kezelésÁraDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kezelésIdőpontjaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kezeltVendégNeveDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.alkalmazottSzakmájaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.alkalmazottNeveDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kezelésMegnevezéseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.szakmakBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSourceAllDataForTable)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.szepsegszalonDataSet)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szakmakBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szakmakBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // bindingSourceAllDataForTable
@@ -71,8 +78,10 @@ namespace Szepsegszalon_WinForm_PPP
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 1);
             this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -85,12 +94,13 @@ namespace Szepsegszalon_WinForm_PPP
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1075, 3);
+            this.button1.Location = new System.Drawing.Point(1029, 168);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView1
             // 
@@ -118,57 +128,38 @@ namespace Szepsegszalon_WinForm_PPP
             this.tableLayoutPanel1.SetRowSpan(this.dataGridView1, 3);
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1046, 492);
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 492);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // kezelésMegnevezéseDataGridViewTextBoxColumn
+            // comboBox1
             // 
-            this.kezelésMegnevezéseDataGridViewTextBoxColumn.DataPropertyName = "Kezelés megnevezése";
-            this.kezelésMegnevezéseDataGridViewTextBoxColumn.HeaderText = "Kezelés megnevezése";
-            this.kezelésMegnevezéseDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.kezelésMegnevezéseDataGridViewTextBoxColumn.Name = "kezelésMegnevezéseDataGridViewTextBoxColumn";
-            this.kezelésMegnevezéseDataGridViewTextBoxColumn.ReadOnly = true;
+            this.comboBox1.DataSource = this.szakmakBindingSource1;
+            this.comboBox1.DisplayMember = "nev";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1029, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.ValueMember = "nev";
             // 
-            // alkalmazottNeveDataGridViewTextBoxColumn
+            // szakmakBindingSource
             // 
-            this.alkalmazottNeveDataGridViewTextBoxColumn.DataPropertyName = "Alkalmazott neve";
-            this.alkalmazottNeveDataGridViewTextBoxColumn.HeaderText = "Alkalmazott neve";
-            this.alkalmazottNeveDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.alkalmazottNeveDataGridViewTextBoxColumn.Name = "alkalmazottNeveDataGridViewTextBoxColumn";
-            this.alkalmazottNeveDataGridViewTextBoxColumn.ReadOnly = true;
+            this.szakmakBindingSource.DataMember = "szakmak";
+            this.szakmakBindingSource.DataSource = this.szepsegszalonDataSet;
             // 
-            // alkalmazottTelefonszámaDataGridViewTextBoxColumn
+            // szakmakTableAdapter
             // 
-            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.DataPropertyName = "Alkalmazott telefonszáma";
-            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.HeaderText = "Alkalmazott telefonszáma";
-            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.Name = "alkalmazottTelefonszámaDataGridViewTextBoxColumn";
-            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.ReadOnly = true;
+            this.szakmakTableAdapter.ClearBeforeFill = true;
             // 
-            // alkalmazottSzakmájaDataGridViewTextBoxColumn
+            // kezelésÁraDataGridViewTextBoxColumn
             // 
-            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.DataPropertyName = "Alkalmazott szakmája";
-            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.HeaderText = "Alkalmazott szakmája";
-            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.Name = "alkalmazottSzakmájaDataGridViewTextBoxColumn";
-            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // kezeltVendégNeveDataGridViewTextBoxColumn
-            // 
-            this.kezeltVendégNeveDataGridViewTextBoxColumn.DataPropertyName = "Kezelt vendég neve";
-            this.kezeltVendégNeveDataGridViewTextBoxColumn.HeaderText = "Kezelt vendég neve";
-            this.kezeltVendégNeveDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.kezeltVendégNeveDataGridViewTextBoxColumn.Name = "kezeltVendégNeveDataGridViewTextBoxColumn";
-            this.kezeltVendégNeveDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // kezeltVendégTelefonszámaDataGridViewTextBoxColumn
-            // 
-            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.DataPropertyName = "Kezelt vendég telefonszáma";
-            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.HeaderText = "Kezelt vendég telefonszáma";
-            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.Name = "kezeltVendégTelefonszámaDataGridViewTextBoxColumn";
-            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.ReadOnly = true;
+            this.kezelésÁraDataGridViewTextBoxColumn.DataPropertyName = "Kezelés ára";
+            this.kezelésÁraDataGridViewTextBoxColumn.HeaderText = "Kezelés ára";
+            this.kezelésÁraDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.kezelésÁraDataGridViewTextBoxColumn.Name = "kezelésÁraDataGridViewTextBoxColumn";
+            this.kezelésÁraDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // kezelésIdőpontjaDataGridViewTextBoxColumn
             // 
@@ -178,13 +169,68 @@ namespace Szepsegszalon_WinForm_PPP
             this.kezelésIdőpontjaDataGridViewTextBoxColumn.Name = "kezelésIdőpontjaDataGridViewTextBoxColumn";
             this.kezelésIdőpontjaDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // kezelésÁraDataGridViewTextBoxColumn
+            // kezeltVendégTelefonszámaDataGridViewTextBoxColumn
             // 
-            this.kezelésÁraDataGridViewTextBoxColumn.DataPropertyName = "Kezelés ára";
-            this.kezelésÁraDataGridViewTextBoxColumn.HeaderText = "Kezelés ára";
-            this.kezelésÁraDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.kezelésÁraDataGridViewTextBoxColumn.Name = "kezelésÁraDataGridViewTextBoxColumn";
-            this.kezelésÁraDataGridViewTextBoxColumn.ReadOnly = true;
+            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.DataPropertyName = "Kezelt vendég telefonszáma";
+            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.HeaderText = "Kezelt vendég telefonszáma";
+            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.Name = "kezeltVendégTelefonszámaDataGridViewTextBoxColumn";
+            this.kezeltVendégTelefonszámaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // kezeltVendégNeveDataGridViewTextBoxColumn
+            // 
+            this.kezeltVendégNeveDataGridViewTextBoxColumn.DataPropertyName = "Kezelt vendég neve";
+            this.kezeltVendégNeveDataGridViewTextBoxColumn.HeaderText = "Kezelt vendég neve";
+            this.kezeltVendégNeveDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.kezeltVendégNeveDataGridViewTextBoxColumn.Name = "kezeltVendégNeveDataGridViewTextBoxColumn";
+            this.kezeltVendégNeveDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // alkalmazottSzakmájaDataGridViewTextBoxColumn
+            // 
+            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.DataPropertyName = "Alkalmazott szakmája";
+            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.HeaderText = "Alkalmazott szakmája";
+            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.Name = "alkalmazottSzakmájaDataGridViewTextBoxColumn";
+            this.alkalmazottSzakmájaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // alkalmazottTelefonszámaDataGridViewTextBoxColumn
+            // 
+            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.DataPropertyName = "Alkalmazott telefonszáma";
+            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.HeaderText = "Alkalmazott telefonszáma";
+            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.Name = "alkalmazottTelefonszámaDataGridViewTextBoxColumn";
+            this.alkalmazottTelefonszámaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // alkalmazottNeveDataGridViewTextBoxColumn
+            // 
+            this.alkalmazottNeveDataGridViewTextBoxColumn.DataPropertyName = "Alkalmazott neve";
+            this.alkalmazottNeveDataGridViewTextBoxColumn.HeaderText = "Alkalmazott neve";
+            this.alkalmazottNeveDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.alkalmazottNeveDataGridViewTextBoxColumn.Name = "alkalmazottNeveDataGridViewTextBoxColumn";
+            this.alkalmazottNeveDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // kezelésMegnevezéseDataGridViewTextBoxColumn
+            // 
+            this.kezelésMegnevezéseDataGridViewTextBoxColumn.DataPropertyName = "Kezelés megnevezése";
+            this.kezelésMegnevezéseDataGridViewTextBoxColumn.HeaderText = "Kezelés megnevezése";
+            this.kezelésMegnevezéseDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.kezelésMegnevezéseDataGridViewTextBoxColumn.Name = "kezelésMegnevezéseDataGridViewTextBoxColumn";
+            this.kezelésMegnevezéseDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // szakmakBindingSource1
+            // 
+            this.szakmakBindingSource1.DataMember = "szakmak";
+            this.szakmakBindingSource1.DataSource = this.szepsegszalonDataSet;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1029, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -199,6 +245,8 @@ namespace Szepsegszalon_WinForm_PPP
             ((System.ComponentModel.ISupportInitialize)(this.szepsegszalonDataSet)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szakmakBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szakmakBindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -211,6 +259,9 @@ namespace Szepsegszalon_WinForm_PPP
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.BindingSource szakmakBindingSource;
+        private szepsegszalonDataSetTableAdapters.szakmakTableAdapter szakmakTableAdapter;
         private System.Windows.Forms.DataGridViewTextBoxColumn kezelésMegnevezéseDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn alkalmazottNeveDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn alkalmazottTelefonszámaDataGridViewTextBoxColumn;
@@ -219,6 +270,8 @@ namespace Szepsegszalon_WinForm_PPP
         private System.Windows.Forms.DataGridViewTextBoxColumn kezeltVendégTelefonszámaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn kezelésIdőpontjaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn kezelésÁraDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource szakmakBindingSource1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Szepsegszalon_WinForm_PPP/Szepsegszalon_WinForm_PPP/Form1.cs
+++ Szepsegszalon_WinForm_PPP/Szepsegszalon_WinForm_PPP/Form1.cs
@@ -19,6 +19,8 @@ namespace Szepsegszalon_WinForm_PPP
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'szepsegszalonDataSet.szakmak' table. You can move, or remove it, as needed.
+            this.szakmakTableAdapter.Fill(this.szepsegszalonDataSet.szakmak);
             // TODO: This line of code loads data into the 'szepsegszalonDataSet.AllData' table. You can move, or remove it, as needed.
             this.allDataTableAdapter.Fill(this.szepsegszalonDataSet.AllData);
 
@@ -27,6 +29,16 @@ namespace Szepsegszalon_WinForm_PPP
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bindingSourceAllDataForTable.Filter = $"Kezelés megnevezése='{comboBox1.SelectedValue}'";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bindingSourceAllDataForTable.Filter = "";
         }
     }
 }
